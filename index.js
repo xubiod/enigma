01988e09dd20eb351f13a83b603cbba230503a05
Xubiod <xubiod@users.noreply.github.com>
2018-05-15 13:26:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -53,9 +53,23 @@ function generateFakes(amt) {
   }
 }
 
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    throw "Query var not found";
+}
+
 //the goooooood stuff
 function loadDone(url) {
-  //loadSVG();
+  try {
+      url = getQueryVariable('src');
+  } catch(err) { /* proceed as normal */ }
   document.getElementById("loadingscreen").style.display = "inital";
   
   //read from json
