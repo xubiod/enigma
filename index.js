89381798107d501050ceb567164218cdf2aee86b
Xubiod <xubiod@users.noreply.github.com>
2018-05-15 13:29:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //variable declaration
 var totalFakes = 0;
+var firsttime = true;
 if (true) {var approveSVG = `<span title="Verified"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
     <path fill="#fff" d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z" />
 </svg></span>`;
@@ -67,9 +68,12 @@ function getQueryVariable(variable) {
 
 //the goooooood stuff
 function loadDone(url) {
-  try {
-      url = getQueryVariable('src');
-  } catch(err) { /* proceed as normal */ }
+    if (firstTime) {
+        try {
+            url = getQueryVariable('src');
+        } catch(err) { /* proceed as normal */ }
+        firstTime = false;
+    }
   document.getElementById("loadingscreen").style.display = "inital";
   
   //read from json
