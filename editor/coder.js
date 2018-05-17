d0b918245da07a4daa1fc9528713ac343195ad47
xubiod.mail <xubiod.mail@gmail.com>
2018-05-17 12:41:08 -0400

    full editor functionality

--- editor/coder.js
+++ editor/coder.js
@@ -1,4 +1,4 @@
-document.getElementById("editor").innerHTML = `{
+document.getElementById("editor-text").innerHTML = `{
     "titlecard": "Your title here", 
     "results": [ 
         { 
@@ -11,9 +11,34 @@ document.getElementById("editor").innerHTML = `{
     ]
 }`
 
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/json");
-editor.on("change", function() {
-    loaded();
-});
+var editor;
+var _editing = true;
+
+function ace() {
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.session.setMode("ace/mode/json");
+  editor.setOptions({
+    foldStyle: "markbeginend"
+  });
+
+  var ele = document.getElementsByClassName("ace_content")
+  for (var q = 0; q < ele.length; q++) {
+    var _ele = ele.item(q);
+    ele.style.fontFamily = "monospace";
+  }
+}
+
+function codemirror() {
+  editor = CodeMirror.fromTextArea(document.getElementById("editor-text"), {
+    lineNumbers: true,
+    mode: "javascript",
+    theme: document.getElementById("theme-picker").value
+  });
+}
+
+function changeTheme() {
+  editor.setOption('theme', document.getElementById('theme-picker').value);
+}
+
+codemirror();
